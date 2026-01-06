a37ef7165c279684ddf83479aa7028dbf15a9854
Karen Gutierrez <karennilda45@gmail.com>
2026-01-06 17:53:50 -0300

    comente logica de pwa en vot

--- vite.config.js
+++ vite.config.js
@@ -5,34 +5,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-        VitePWA({
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) =>
-              url.pathname.startsWith('/api/'),
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24
-              }
-            }
-          },
-          {
-            urlPattern: ({ request }) =>
-              request.destination === 'image',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images-cache',
-              expiration: {
-                maxEntries: 200
-              }
-            }
-          }
-        ]
-      }
-    })
+    //     VitePWA({
+    //   workbox: {
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: ({ url }) =>
+    //           url.pathname.startsWith('/api/'),
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'api-cache',
+    //           expiration: {
+    //             maxEntries: 100,
+    //             maxAgeSeconds: 60 * 60 * 24
+    //           }
+    //         }
+    //       },
+    //       {
+    //         urlPattern: ({ request }) =>
+    //           request.destination === 'image',
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'images-cache',
+    //           expiration: {
+    //             maxEntries: 200
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // })
   ]
 })
